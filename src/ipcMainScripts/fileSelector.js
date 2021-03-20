f3a391286c1826773319beb86cfefb48950baf4c
Connor Walker <connorjwalker32@hotmail.co.uk>
2021-03-20 23:22:22 +0000

    added file reader and error display

--- src/ipcMainScripts/fileSelector.js
+++ src/ipcMainScripts/fileSelector.js
@@ -1,4 +1,6 @@
 const { dialog } = require('electron')
+const fs = require('fs')
+const path = require('path')
 
 exports.AddEventListeners = ipcMain => {
     ipcMain.on('open-file-dialogue', event => {
@@ -7,10 +9,24 @@ exports.AddEventListeners = ipcMain => {
                 event.sender.send('message', { message: 'ipc-loaded-file', data: file })
             })
     })
+
+    ipcMain.on('get-recent-files', event => {
+        const responseName = 'ipc-recent-files'
+        fs.readFile(path.join(__dirname, '../../assets/recentFiles.json'), 'utf8', (err, file) => {
+            if (err) {
+                console.error(err)
+                event.sender.send('message', { message: responseName, data: { success: false }})
+                return
+            }
+
+            file = JSON.parse(file)
+            event.sender.send('message', { message: responseName, data: { success: true, file }})
+        })
+    })
 }
 
 exports.RemoveEventListeners = ipcMain => {
-    ['open-file-dialogue'].forEach(listener => {
+    ['open-file-dialogue', 'get-recent-files'].forEach(listener => {
         ipcMain.removeListener(listener)
     })
 }
--- src/ui/scripts/fileSelector.js
+++ src/ui/scripts/fileSelector.js
@@ -1,11 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
     const filepathLbl = document.getElementById('filepath-lbl')
-    
+    const fileErrorLbl = document.getElementById('recent-file-errors')
+
     document.getElementById('search-btn').addEventListener('click', () => {
         window.postMessage({ message: 'open-file-dialogue' })
     })
 
     window.addEventListener('ipc-loaded-file', event => {
-        console.log(event)
+        if (event.detail.canceled) return
+
+        filepathLbl.innerText = event.detail.filePaths[0]
     })
+
+    window.addEventListener('ipc-recent-files', event => {
+        if (!event.detail.success) {
+            fileErrorLbl.innerText = 'Could not load recent files :('
+            return
+        }
+        else if (event.detail.file.recents.length === 0) {
+            fileErrorLbl.innerText = 'No recent files :('
+            return
+        }
+    })
+
+    window.postMessage({ message: 'get-recent-files' })
 })
