908291d3b46651a3200ec60fdd60f3381b2c8909
Connor Walker <connorjwalker32@hotmail.co.uk>
2021-03-21 04:58:27 +0000

    added recent files update function

--- src/ipcMainScripts/fileSelector.js
+++ src/ipcMainScripts/fileSelector.js
@@ -3,6 +3,9 @@ const { spawn } = require('child_process')
 const fs = require('fs')
 const path = require('path')
 
+let recentFiles
+const recentFilesPath = path.join(__dirname, '../../assets/recentFiles.json')
+
 exports.AddEventListeners = ipcMain => {
     ipcMain.on('open-file-dialogue', event => {
         dialog.showOpenDialog({ properties: ['openFile'] })
@@ -13,15 +16,15 @@ exports.AddEventListeners = ipcMain => {
 
     ipcMain.on('get-recent-files', event => {
         const responseName = 'ipc-recent-files'
-        fs.readFile(path.join(__dirname, '../../assets/recentFiles.json'), 'utf8', (err, file) => {
+        fs.readFile(recentFilesPath, 'utf8', (err, file) => {
             if (err) {
                 console.error(err)
                 event.sender.send('message', { message: responseName, data: { success: false }})
                 return
             }
 
-            file = JSON.parse(file)
-            event.sender.send('message', { message: responseName, data: { success: true, file }})
+            recentFiles = JSON.parse(file)
+            event.sender.send('message', { message: responseName, data: { success: true, file: recentFiles }})
         })
     })
 
@@ -32,14 +35,35 @@ exports.AddEventListeners = ipcMain => {
 
         python.stdout.on('data', data => outputString += data.toString())
         python.stderr.on('data', err => console.error(err.toString()))
-        python.on('exit', () =>  {
-            event.sender.send('message', { message: 'ipc-open-gp-file', data: JSON.parse(outputString) })
-        })
+        python.on('exit', () =>  updateRecentFiles(event, outputString))
     })
 }
 
 exports.RemoveEventListeners = ipcMain => {
     ['open-file-dialogue', 'get-recent-files', 'open-gp-file'].forEach(listener => {
         ipcMain.removeListener(listener)
+    })
+}
+
+function updateRecentFiles(event, file) {
+    file = JSON.parse(file)
+    const index = recentFiles.recents.findIndex(element => 
+        element.artist === file.artist && element.title === file.artist
+    )
+
+    // Selected song is not in recent files list if index is -1
+    if (index === -1) {
+        if (recentFiles.recents.length >= 5)
+            recentFiles.recents.pop()
+        recentFiles.recents.unshift({ artist: file.artist, title: file.title, date: Date.now() })
+    } else {
+        selected = recentFiles.recents[index]
+        selected.date = Date.now
+        recentFiles.recents.splice(index, 1)
+        recentFiles.recents.unshift(selected)
+    }
+
+    fs.writeFile(recentFilesPath, JSON.stringify(recentFiles), 'utf8', () => {
+        event.sender.send('message', { message: 'ipc-open-gp-file', data: file })
     })
 }
