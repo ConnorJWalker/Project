1ee77f70776107ecc799e11d21d701c550f326e5
Connor Walker <connorjwalker32@hotmail.co.uk>
2021-03-26 18:30:14 +0000

    added tuning rendering

--- src/ui/scripts/tabRenderers/renderer.js
+++ src/ui/scripts/tabRenderers/renderer.js
@@ -3,8 +3,6 @@ class Renderer {
         this.canvas = document.querySelector('canvas')
         this.context = this.canvas.getContext('2d')
 
-        this.currentX = 0
-        this.currentY = 0
         this.canvasWidth = this.getCanvasWidth()
         // TODO: persist settings set by user
         this.settings = {
@@ -18,9 +16,14 @@ class Renderer {
     renderBars(numberOfBars, tuningNotes) {
         // Reset x and y to prevent rerenders apearing off screen
         this.currentX = 0
-        this.currentY = 0
+        this.currentY = 20 // Padding to prevent cut off
         
         for (let i = 0; i < numberOfBars; i++) {
+            if (i === 0) {
+                this.renderBarWithTuningNotes(tuningNotes)
+                continue
+            }
+            
             // TODO: work out height properly without hardcoding
             this.context.strokeRect(this.currentX, this.currentY, this.canvasWidth / 3, this.settings.barHeight)
             this.renderStringsInBar(tuningNotes)
@@ -36,19 +39,37 @@ class Renderer {
         }
     }
 
-    renderStringsInBar(tuningNotes) {
+    renderStringsInBar(tuningNotes, start) {
         this.context.beginPath()
         const height = this.settings.barHeight / (tuningNotes.length - 1)
         let currenyY = this.currentY + height
+        start = start || 0
 
         for (let i = 0; i < tuningNotes.length - 1; i++) {
-            this.context.moveTo(this.currentX, currenyY)
+            this.context.moveTo(this.currentX + start, currenyY)
             this.context.lineTo(this.currentX + this.canvasWidth / this.settings.barsPerLine, currenyY)
             currenyY += height
         }
         
         this.context.closePath()
         this.context.stroke()
+    }
+
+    renderBarWithTuningNotes(tuningNotes) {
+        let longestWidth = 0
+        const height = (this.settings.barHeight / tuningNotes.length) + (parseInt(this.context.font) / 4.6)
+        this.context.textBaseline = 'middle'
+        tuningNotes.forEach((note, i) => {
+            this.context.fillText(note, this.currentX, this.currentY + (height * i))
+            longestWidth = Math.max(longestWidth, Math.ceil(this.context.measureText(note).width))
+        })
+        longestWidth += 10 
+
+        const width = (this.canvasWidth / this.settings.barsPerLine) - longestWidth
+        this.context.strokeRect(this.currentX + longestWidth, this.currentY, width, this.settings.barHeight)
+        this.renderStringsInBar(tuningNotes, longestWidth)
+
+        this.currentX += this.canvasWidth / this.settings.barsPerLine
     }
 
     getCanvasWidth() {
